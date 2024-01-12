894dbfd33d42ca0428af0d4e7b6d99f6a3478bab
Mehdi ÇELİK <105076539+MehdCan98@users.noreply.github.com>
2024-01-12 16:22:14 +0300

    Kod ile buton kontrol

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void changeImage(View view){
+        ImageView metallica = findViewById(R.id.imageView);
+        metallica.setImageResource(R.drawable.indir1);
+    }
+}
